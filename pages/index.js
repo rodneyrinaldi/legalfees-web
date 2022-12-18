3b3965e8d37eeeed9e2022bdfca7f932665c06cd
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2022-12-18 18:48:08 -0300

    adding legal fees table

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import clientPromise from "../lib/mongodb";
 import styles from "../styles/index.module.css";
+import Meta from "./meta";
 
-export default function Legalfees({ legalfees }) {
+export default function Legalfees({ legalfees, query }) {
+  const router = useRouter();
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {}, [filter]);
+
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>dev.rodneyrinaldi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Meta
+        title="Legal Fees"
+        descrition="Tabela de honorários advocatícios"
+        image="https://legalfees.rrs.net.br/card.jpg"
+        url="https://legalfees.rrs.net.br/"
+      />
+
       <div className={styles.placeholdercontent}>
         <section className={styles.stickycontent}>
           <section className={styles.stickycontentimage}>
@@ -21,15 +44,20 @@ export default function Legalfees({ legalfees }) {
           <h1 className={styles.stickycontenttitle}>
             Tabela de honorários advocatícios <span>2022</span>
           </h1>
-          <input type="text" placeholder="pesquisar por ..." />
+          <input
+            type="text"
+            placeholder="pesquisar por ..."
+            onChange={(e) => setFilter(e.target.value)}
+          />
         </section>
-
         {legalfees.map((legalfees) => (
-          <div className={styles.placeholdercard}>
-            <h4>{legalfees.Classe}</h4>
-            <p>{legalfees.Subclasse}</p>
-            <p>{legalfees.Atividade}</p>
-            {legalfees.Valor ? <h4>{legalfees.Valor}</h4> : null}
+          <div className={styles.placeholdercard} key={legalfees._id}>
+            <h3 key={legalfees._id + "a"}>{legalfees.Classe}</h3>
+            <p key={legalfees._id + "b"}>{legalfees.Subclasse}</p>
+            <p key={legalfees._id + "c"}>{legalfees.Atividade}</p>
+            {legalfees.Valor ? (
+              <h2 key={legalfees._id + "d"}>{legalfees.Valor}</h2>
+            ) : null}
           </div>
         ))}
       </div>
@@ -37,15 +65,17 @@ export default function Legalfees({ legalfees }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(query) {
   try {
+    const filter = query ? query.filter : "";
+    console.log(filter);
+
     const client = await clientPromise;
     const db = client.db("legalfees");
-
     const legalfees = await db
       .collection("fees")
-      .find({})
-      // .sort({ Atividade: -1 })
+      .find({ Atividade: { $regex: "", $options: "i" } })
+      // .find({ Atividade: "Parecer ou memorial" })
       .limit(200)
       .toArray();
 
